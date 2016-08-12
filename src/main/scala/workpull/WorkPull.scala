8cb2bc85257ed99a68d0c3832767087f7b060018
Tom Liddle <tliddle@brighttalk.com>
2016-08-12 16:17:58 +0100

    Improve the code comments

--- src/main/scala/workpull/WorkPull.scala
+++ src/main/scala/workpull/WorkPull.scala
@@ -5,7 +5,6 @@ import java.util.concurrent.Executors
 import org.slf4j.LoggerFactory
 import workpull.WorkPull.{Work, _}
 import workpull.Worker.{RequestWork, Result, WorkFailed, WorkerMessage}
-
 import scala.collection.immutable.Queue
 import scala.concurrent.ExecutionContext
 import scalaz.concurrent._
@@ -20,25 +19,27 @@ object WorkPull {
 	case class WorkAvailable() extends ParentMessage
 	case class Work[T](work: T) extends ParentMessage
 
+  private val log = LoggerFactory.getLogger(getClass)
+
+  // OK so we should do some testing. With this setup there isn't much to test
 	def main(args: Array[String]): Unit = {
     val list = (0 to 10000).map(x => s"number $x")
 		val wp = new WorkPull[String, String](100, Queue[String](list: _*), (String) => "result").run()
-
-    //if (wp.active == 0)
-    //  wp.close()
 	}
 }
 
 /**
-	* Main actor which stores urls that have been processed and the queue of urls to process
+	* Class that dishes out the work
+  * Note it *has an* actor rather than being one.
+  * The actor is has is immutable
+  * As opposed to Akka which allows messages of Any => Unit, we can specify which messages are allowed at compile time :)
+  *
  *
 	* @param noOfWorkers no of actors processing urls
 	*/
 class WorkPull[T, U](noOfWorkers: Int, queue: Queue[T], task: (T => U)) {
 
-	private val log = LoggerFactory.getLogger(getClass)
-
-  @volatile private var result = Map[T, U]()
+  @volatile private var _results = Map[T, U]()
   @volatile private var _queue = queue
   @volatile private var active = 0
 
@@ -50,7 +51,8 @@ class WorkPull[T, U](noOfWorkers: Int, queue: Queue[T], task: (T => U)) {
 
         log.debug(s"active count is now $active $res")
 
-        result = result + (id -> res)
+        // Update the results
+        _results = _results + (id -> res)
 
       case w: WorkFailed[T] =>
         log.debug(s"URL failed to be parsed: $w")
@@ -60,8 +62,10 @@ class WorkPull[T, U](noOfWorkers: Int, queue: Queue[T], task: (T => U)) {
       case r: RequestWork =>
         _queue.dequeueOption.foreach {
           case ((head, tail)) =>
+            // Dequeue the work - we will only attempt this once
             _queue = tail
 
+            // Send the work to the worker actor
             r.w.workerActor ! Work(head)
 
             active = active + 1
@@ -71,17 +75,20 @@ class WorkPull[T, U](noOfWorkers: Int, queue: Queue[T], task: (T => U)) {
     }
   }
 
-  def leftToProcess = active
+  def leftToProcess: Int = _queue.length
+
+  def results: Map[T, U] = _results
 
   def close(force: Boolean = false): Unit = {
     if (force || active == 0) {
-      log.debug(result.values.zipWithIndex.mkString(" \n"))
+      log.debug(_results.values.zipWithIndex.mkString(" \n"))
       pool.shutdown()
     }
   }
 
   def run(): WorkPull[T, U] = {
     (1 to noOfWorkers).map(_ => new Worker(parent, task))
+    // Allow chaining
     this
   }
 }
--- src/main/scala/workpull/Worker.scala
+++ src/main/scala/workpull/Worker.scala
@@ -3,7 +3,6 @@ package workpull
 import org.slf4j.LoggerFactory
 import workpull.WorkPull.{ParentMessage, Work, WorkAvailable, _}
 import workpull.Worker._
-
 import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success, Try}
 import scalaz.concurrent.Actor
@@ -11,11 +10,13 @@ import scalaz.concurrent.Actor
 
 
 object Worker {
-  abstract class WorkerMessage(w: AbstractWorker)
+  // A worker needs a reference to its parent
   trait AbstractWorker {
     val workerActor: Actor[ParentMessage]
   }
 
+  // A worker can only send a worker message. The parent can only listen to worker messages.
+  abstract class WorkerMessage(w: AbstractWorker)
   case class RequestWork(w: AbstractWorker) extends WorkerMessage(w)
   case class WorkFailed[T](w: AbstractWorker, url: T) extends WorkerMessage(w)
   case class Result[T, U](w: AbstractWorker, id: T, result: U) extends WorkerMessage(w)
@@ -24,22 +25,25 @@ object Worker {
 
 
 /**
-  * Fetches the resource from the url given in the work message. Sends a message to the parent with the parsed links
+  * Does the work and returns the result. Simple(z)
+  * Note there isn't much logic in here so we can test the task separately. No need to roll out akka test ;)
   *
   */
 class Worker[T, U](parent: Actor[WorkerMessage], task: (T => U))(implicit val ec: ExecutionContext) extends AbstractWorker {
 
-
   private val log = LoggerFactory.getLogger(getClass)
 
+  // Request the work once we start up
   parent ! RequestWork(this)
 
 
   override val workerActor: Actor[ParentMessage] = Actor.actor {
     (p: ParentMessage) => p match {
+      // We have received a message that work is available. Request it
       case w: WorkAvailable =>
         parent ! RequestWork(this)
 
+      // We have received some work, do it and return the results
       case Work(key: T) =>
         Try(task(key)) match {
           // A successful retrieval, notify the master
